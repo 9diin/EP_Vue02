2da116688dd8109381a0264f6af00446d553e2ec
9diin <weartstudio@naver.com>
2024-11-30 15:35:58 +0900

    #11-HourlyWeather 위젯 UI 완성

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -1,2 +1,3 @@
 export { default as CommonHeader } from "./header/Header.vue";
 export { default as WidgetCurrentWeather } from "./widgets/CurrentWeather.vue";
+export { default as WidgetHourlyWeather } from "./widgets/HourlyWeather.vue";
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,6 +5,7 @@
             <div className="w-full h-full flex flex-col items-center justify-start pb-6 px-6 gap-6">
                 <div className="w-full flex items-flex justify-start gap-6">
                     <WidgetCurrentWeather />
+                    <WidgetHourlyWeather />
                 </div>
                 <div className="w-full flex items-center gap-6"></div>
             </div>
@@ -13,7 +14,7 @@
 </template>
 
 <script setup lang="ts">
-import { CommonHeader, WidgetCurrentWeather } from "@/components/common";
+import { CommonHeader, WidgetCurrentWeather, WidgetHourlyWeather } from "@/components/common";
 </script>
 
 <style lang="scss" scoped></style>
