90ad1c23231dfcf49220b00fe1537d6adb4305f9
9diin <weartstudio@naver.com>
2024-12-12 11:12:48 +0900

    ☘️ [feat] WidgetHighlightWeather 데이터 바인딩 완료

--- src/components/widgets/HighlightWeather.vue
+++ src/components/widgets/HighlightWeather.vue
@@ -19,19 +19,19 @@
                         <div class="w-fit grid grid-cols-4 gap-3">
                             <div class="flex flex-col items-center">
                                 <p class="text-sm text-muted-foreground">1회 - 만조</p>
-                                <p class="poppins-medium scroll-m-20 text-lg font-semibold tracking-tight">06:30<span class="ml-[1px]">am</span></p>
+                                <p class="poppins-medium scroll-m-20 text-lg font-semibold tracking-tight">{{ data.moonset }}</p>
                             </div>
                             <div class="flex flex-col items-center">
-                                <p class="text-sm text-muted-foreground">1회 - 만조</p>
-                                <p class="poppins-medium scroll-m-20 text-lg font-semibold tracking-tight">06:30<span class="ml-[1px]">am</span></p>
+                                <p class="text-sm text-muted-foreground">2회 - 간조</p>
+                                <p class="poppins-medium scroll-m-20 text-lg font-semibold tracking-tight">{{ data.sunrise }}</p>
                             </div>
                             <div class="flex flex-col items-center">
-                                <p class="text-sm text-muted-foreground">1회 - 만조</p>
-                                <p class="poppins-medium scroll-m-20 text-lg font-semibold tracking-tight">06:30<span class="ml-[1px]">am</span></p>
+                                <p class="text-sm text-muted-foreground">3회 - 만조</p>
+                                <p class="poppins-medium scroll-m-20 text-lg font-semibold tracking-tight">{{ data.moonrise }}</p>
                             </div>
                             <div class="flex flex-col items-center">
-                                <p class="text-sm text-muted-foreground">1회 - 만조</p>
-                                <p class="poppins-medium scroll-m-20 text-lg font-semibold tracking-tight">06:30<span class="ml-[1px]">am</span></p>
+                                <p class="text-sm text-muted-foreground">4회 - 간조</p>
+                                <p class="poppins-medium scroll-m-20 text-lg font-semibold tracking-tight">{{ data.sunset }}</p>
                             </div>
                         </div>
                     </CardContent>
@@ -49,14 +49,14 @@
                             <img src="/assets/icons/1000d.svg" alt="" class="h-14" />
                             <div class="flex flex-col">
                                 <p class="text-sm text-muted-foreground">Sunrise</p>
-                                <p class="poppins-medium scroll-m-20 text-3xl font-semibold tracking-tight">05:30</p>
+                                <p class="poppins-medium scroll-m-20 text-3xl font-semibold tracking-tight">{{ data.sunrise }}</p>
                             </div>
                         </div>
                         <div class="w-full flex items-center gap-2">
                             <img src="/assets/icons/1000n.svg" alt="" class="h-14" />
                             <div class="flex flex-col">
                                 <p class="text-sm text-muted-foreground">Sunset</p>
-                                <p class="poppins-medium scroll-m-20 text-3xl font-semibold tracking-tight">18:30</p>
+                                <p class="poppins-medium scroll-m-20 text-3xl font-semibold tracking-tight">{{ data.sunset }}</p>
                             </div>
                         </div>
                     </CardContent>
@@ -73,7 +73,7 @@
                     <CardContent class="flex items-center justify-between">
                         <img src="/assets/images/humidity.svg" alt="" class="h-10 w-10" />
                         <p class="poppins-medium scroll-m-20 text-3xl font-semibold tracking-tight">
-                            80
+                            {{ data.humidity }}
                             <span class="text-lg ml-1">%</span>
                         </p>
                     </CardContent>
@@ -88,7 +88,7 @@
                     <CardContent class="flex items-center justify-between">
                         <img src="/assets/images/wind.svg" alt="" class="h-10 w-10" />
                         <p class="poppins-medium scroll-m-20 text-3xl font-semibold tracking-tight">
-                            1024
+                            {{ data.pressure }}
                             <span class="text-lg ml-1">hPa</span>
                         </p>
                     </CardContent>
@@ -103,7 +103,7 @@
                     <CardContent class="flex items-center justify-between">
                         <img src="/assets/images/fog.svg" alt="" class="h-10 w-10" />
                         <p class="poppins-medium scroll-m-20 text-3xl font-semibold tracking-tight">
-                            10
+                            {{ data.visibility }}
                             <span class="text-lg ml-1">km</span>
                         </p>
                     </CardContent>
@@ -118,7 +118,7 @@
                     <CardContent class="flex items-center justify-between">
                         <img src="/assets/images/hot.svg" alt="" class="h-10 w-10" />
                         <p class="poppins-medium scroll-m-20 text-3xl font-semibold tracking-tight flex items-start">
-                            10
+                            {{ data.feelsLike }}
                             <span class="text-lg ml-[2px] -mt-[2px]">&#8451;</span>
                         </p>
                     </CardContent>
@@ -130,4 +130,19 @@
 
 <script setup lang="ts">
 import { Card, CardHeader, CardTitle, CardDescription, CardContent } from "@/components/ui/card";
+import { toRefs } from "vue";
+
+interface HighlightData {
+    sunrise: string;
+    sunset: string;
+    moonrise: string;
+    moonset: string;
+    humidity: number;
+    pressure: number;
+    visibility: number;
+    feelsLike: number;
+}
+
+const props = defineProps<{ data: HighlightData }>();
+const { data } = toRefs(props);
 </script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,13 +7,13 @@
                 <!-- 상단 위젯 영역 -->
                 <div class="w-full flex items-flex justify-start gap-6">
                     <WidgetCurrentWeather :data="dataOfCW" />
-                    <WidgetHourlyWeather />
+                    <WidgetHourlyWeather :data="dataOfHW" />
                     <!-- 네이버 지도 위젯 -->
                     <WidgetNaverMap />
                 </div>
                 <!-- 하단 위젯 영역 -->
                 <div class="w-full flex items-start gap-6">
-                    <WidgetHighlightWeather />
+                    <WidgetHighlightWeather :data="dataOfHLW" />
                     <WidgetOneWeekWeather />
                 </div>
             </div>
@@ -31,20 +31,36 @@ const WEATHER_BASE_URL = "http://api.weatherapi.com/v1";
 const WEATHER_API_KEY = "56442fada1144d12abf64743241411";
 
 const cityName = ref<string>("seoul");
-const dataOfCW = reactive({ temp: 0, country: "", cityName: "", localtime: "", iconCode: 0, iconUrl: "" }); // WidgetCurrentWeather 컴포넌트에서 필요한 데이터
+const dataOfCW = reactive({ temp: 0, country: "", cityName: "", localtime: "", iconCode: 0, iconUrl: "" });
+const dataOfHW = ref([]); // WidgetHourlyWeather 컴포넌트에서 사용될 데이터
+const dataOfHLW = reactive({ sunrise: "", sunset: "", moonrise: "", moonset: "", humidity: 0, pressure: 0, visibility: 0, feelsLike: 0 }); // WidgetHighlistWeather 컴포넌트에서 사용될 데이터
 
 const fetchApi = async () => {
     try {
         const res = await axios.get(`${WEATHER_BASE_URL}/forecast.json?q=${cityName.value}&days=7&key=${WEATHER_API_KEY}`);
+        const { current, location, forecast } = res.data;
         console.log(res);
+
         /** WidgetCurrentWeather 컴포넌트에서 필요한 데이터 */
-        const { current, location } = res.data;
         dataOfCW.temp = current.temp_c;
         dataOfCW.country = location.country;
         dataOfCW.cityName = location.name;
         dataOfCW.localtime = location.localtime;
         dataOfCW.iconCode = current.condition.code;
         dataOfCW.iconUrl = current.condition.icon;
+
+        /** WidgetHourlyWeather 컴포넌트에서 필요한 데이터 */
+        dataOfHW.value = forecast.forecastday[0].hour;
+
+        /** WidgetHighlightWeather 컴포넌트에서 필요한 데이터 */
+        dataOfHLW.sunrise = forecast.forecastday[0].astro.sunrise;
+        dataOfHLW.sunset = forecast.forecastday[0].astro.sunset;
+        dataOfHLW.moonrise = forecast.forecastday[0].astro.moonrise;
+        dataOfHLW.moonset = forecast.forecastday[0].astro.moonset;
+        dataOfHLW.humidity = current.humidity;
+        dataOfHLW.pressure = current.pressure_mb;
+        dataOfHLW.visibility = current.vis_km;
+        dataOfHLW.feelsLike = current.feelslike_c;
     } catch (error) {
         console.log("error: ", error);
     }
