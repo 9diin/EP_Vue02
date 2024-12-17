982c97ba670f7a24ad614508149f4381ca3ec1a1
9diin <weartstudio@naver.com>
2024-12-18 02:41:49 +0900

    ☘️ [feat] OneWeekWeather 컴포넌트 데이터 바인딩 완료

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -14,7 +14,7 @@
                 <!-- 하단 위젯 영역 -->
                 <div class="w-full flex items-start gap-6">
                     <WidgetHighlightWeather :data="dataOfHLW" />
-                    <WidgetOneWeekWeather />
+                    <WidgetOneWeekWeather :data="dataOf7DW" />
                 </div>
             </div>
         </div>
@@ -27,19 +27,31 @@ import { CommonHeader } from "@/components/common";
 import { WidgetCurrentWeather, WidgetHourlyWeather, WidgetHighlightWeather, WidgetOneWeekWeather, WidgetNaverMap } from "@/components/widgets";
 import { onMounted, reactive, ref } from "vue";
 
+interface ForecastDay {
+    day: {
+        maxtemp_c: number;
+        mintemp_c: number;
+        condition: {
+            code: number;
+            icon: string;
+        };
+    };
+    date_epoch: string;
+}
+
 const WEATHER_BASE_URL = "http://api.weatherapi.com/v1";
-const WEATHER_API_KEY = "56442fada1144d12abf64743241411";
+const WEATHER_API_KEY = "1c8579f0c8b348fbb22120227241612";
 
 const cityName = ref<string>("seoul");
 const dataOfCW = reactive({ temp: 0, country: "", cityName: "", localtime: "", iconCode: 0, iconUrl: "" });
 const dataOfHW = ref([]);
 const dataOfHLW = reactive({ sunrise: "", sunset: "", moonrise: "", moonset: "", humidity: 0, pressure: 0, visibility: 0, feelsLike: 0 });
+const dataOf7DW = ref([]);
 
 const fetchApi = async () => {
     try {
         const res = await axios.get(`${WEATHER_BASE_URL}/forecast.json?q=${cityName.value}&days=7&key=${WEATHER_API_KEY}`);
         const { current, location, forecast } = res.data;
-        console.log(res);
 
         /** WidgetCurrentWeather 컴포넌트에서 필요한 데이터 */
         dataOfCW.temp = current.temp_c;
@@ -61,6 +73,17 @@ const fetchApi = async () => {
         dataOfHLW.pressure = current.pressure_mb;
         dataOfHLW.visibility = current.vis_km;
         dataOfHLW.feelsLike = current.feelslike_c;
+
+        /** WidgetOneWeekWeather 컴포넌트에서 필요한 데이터 */
+        dataOf7DW.value = forecast.forecastday.map((item: ForecastDay) => {
+            return {
+                maxTemp: Math.round(item.day.maxtemp_c),
+                minTemp: Math.round(item.day.mintemp_c),
+                date: item.date_epoch,
+                iconCode: item.day.condition.code,
+                isDay: item.day.condition.icon.includes("day"),
+            };
+        });
     } catch (error) {
         console.log("error: ", error);
     }
