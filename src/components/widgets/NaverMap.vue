758fa6c7c1d918d4b00072a2773de1b5f40d1cc3
9diin <weartstudio@naver.com>
2024-12-18 21:47:06 +0900

    🔥 프로젝트 개발 완료

--- src/components/widgets/NaverMap.vue
+++ src/components/widgets/NaverMap.vue
@@ -1,15 +1,34 @@
 <template>
     <Card class="w-1/4 min-w-[25%]">
-        <naver-map style="width: 100%; height: 100%" @onLoad="onLoadMap($event)" />
+        <naver-map style="width: 100%; height: 100%" :mapOptions="mapOptions">
+            <naver-marker v-for="position in positions" :latitude="position.lat" :longitude="position.lng" @onLoad="onLoadMarker($event)" @click="handleSearch(position.cityName)"> </naver-marker>
+        </naver-map>
     </Card>
 </template>
 
 <script setup>
+import { ref } from "vue";
+import { NaverMap, NaverMarker } from "vue3-naver-maps";
+import { useStore } from "@/store";
 import { Card } from "@/components/ui/card";
-import { NaverMap } from "vue3-naver-maps";
+import positions from "@/constants/map-position.json";
 
-const onLoadMap = (map) => {
-    const latLng = new window.naver.maps.LatLng(37.51347, 127.041722);
-    map.setCenter(latLng); // Change Map Center
+const store = useStore();
+
+const marker = ref();
+const onLoadMarker = (markerObject) => {
+    marker.value = markerObject;
+};
+const mapOptions = {
+    latitude: 37.51347, // 지도 중앙 위도
+    longitude: 127.041722, // 지도 중앙 경도
+    zoom: 8,
+    zoomControl: false,
+    zoomControlOptions: { position: "TOP_RIGHT" },
+};
+
+const handleSearch = (selected) => {
+    store.cityName = selected;
+    store.fetchApi();
 };
 </script>
